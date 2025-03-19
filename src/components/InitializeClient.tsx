8de172023f430c5d61d6c6c580c37165f1d03246
JaeHo Song <thruthesky@gmail.com>
2025-03-19 14:27:31 +0800

    update on firebsae initialization

--- src/components/InitializeClient.tsx
+++ src/components/InitializeClient.tsx
@@ -3,10 +3,19 @@
 import { useEffect, useState } from "react";
 // Import the functions you need from the SDKs you need
 import { FirebaseApp, getApp, initializeApp } from "firebase/app";
-import { firebaseConfig } from "@/keys/keys";
 
 export default function InitializeClient({ children }: Readonly<{ children: React.ReactNode }>) {
   const [app, setApp] = useState<FirebaseApp | null>(null);
+
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
 
   useEffect(() => {
     // This is a placeholder for any client-side initialization logic you want to run
