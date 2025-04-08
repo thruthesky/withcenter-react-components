0ecf84697b97707da683b86adae73f92f40ef070
JaeHo Song <thruthesky@gmail.com>
2025-04-08 10:23:14 +0800

    updating philgo common ui. updating loading, updating files and submit button

---
+++ Spinner.tsx
@@ -0,0 +1,20 @@
+import { SizeProp } from "@fortawesome/fontawesome-svg-core";
+import { faCircleNotch } from "@fortawesome/pro-light-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Spinner({
+  className,
+  size,
+}: {
+  className?: string;
+  size?: SizeProp;
+}) {
+  return (
+    <FontAwesomeIcon
+      icon={faCircleNotch}
+      className={`spinner w-6 h-6 ${className}`}
+      size={size}
+    />
+  );
+}
+// Spinner component that displays a loading spinner icon.
