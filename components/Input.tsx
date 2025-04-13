10c454c93093e6c94b9bc23d4f04b3601e5875e8
JaeHo Song <thruthesky@gmail.com>
2025-04-13 13:39:04 +0800

    feat: enhance Input component with error message display and label handling

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,7 +1,29 @@
+/**
+ * Input component for text input fields.
+ *
+ * @param params - The props for the Input component.
+ * @param params.type - The type of the input field (default: "text").
+ * @param params.name - The name of the input field.
+ * @param params.required - Whether the input field is required (default: false).
+ * @param params.label - The label for the input field.
+ *   - if label is not provided, the placeholder or name will be used as the label.
+ * @param params.placeholder - The placeholder text for the input field.
+ * @param params.onChange - The function to call when the input value changes.
+ * @param params.onBlur - The function to call when the input field loses focus.
+ * @param params.className - Additional CSS classes for the input field.
+ * @param params.id - The id of the input field.
+ * @param params.value - The controlled value of the input field.
+ * @param params.defaultValue - The default value of the input field.
+ * @param params.errorMessage - The error message to display if there is an error.
+ * @param params.ref - The ref for the input field.
+ * @returns
+ */
 export default function Input({
   type = "text",
   name = "",
   required = false,
+
+  label = "",
   placeholder = "",
   onChange,
   onBlur,
@@ -9,11 +31,13 @@ export default function Input({
   id = "",
   value,
   defaultValue = "",
+  errorMessage,
   ref,
 }: {
   type?: string;
   name?: string;
   required?: boolean;
+  label?: string;
   placeholder?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
@@ -21,21 +45,28 @@ export default function Input({
   id?: string;
   value?: string;
   defaultValue?: string;
+  errorMessage?: string;
   ref?: React.Ref<HTMLInputElement>;
 }) {
   return (
-    <input
-      type={type}
-      name={name}
-      required={required}
-      placeholder={placeholder}
-      id={id}
-      className={`border border-gray-300 rounded-md px-4 py-2 ${className}`}
-      value={value}
-      defaultValue={defaultValue}
-      onChange={onChange}
-      onBlur={onBlur}
-      ref={ref}
-    />
+    <div className="flex flex-col mb-4">
+      <label>
+        <p>{label || placeholder || name}</p>
+        <input
+          type={type}
+          name={name}
+          required={required}
+          placeholder={placeholder}
+          id={id}
+          className={`border border-gray-300 rounded-md px-4 py-2 ${className}`}
+          value={value}
+          defaultValue={defaultValue}
+          onChange={onChange}
+          onBlur={onBlur}
+          ref={ref}
+        />
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+      </label>
+    </div>
   );
 }
